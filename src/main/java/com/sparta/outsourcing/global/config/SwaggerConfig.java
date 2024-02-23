353f3e260a65ae755fe7643857c00a1f3e963d30
Son JunHyung <kingjh1125@gmail.com>
2024-02-23 19:43:26 +0900

    :wrench: add config
    - SwaggerConfig
    - SecurityConfig

--- src/main/java/com/sparta/outsourcing/global/config/SwaggerConfig.java
+++ src/main/java/com/sparta/outsourcing/global/config/SwaggerConfig.java
@@ -1,5 +1,28 @@
 package com.sparta.outsourcing.global.config;
 
-public class SwaggerConfig {
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+            .group("api")
+            .pathsToMatch("/api/**")
+            .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        String title = "outsourcing Application Swagger";
+        String description = "outsourcing Application 팀 프로젝트의 Swagger 문서입니다.";
+
+        Info info = new Info().title(title).description(description).version("1.0.0");
+
+        return new OpenAPI().info(info);
+    }
 }
