635972353d0bb43ddeb9fda44d324d049a762169
Son JunHyung <kingjh1125@gmail.com>
2024-02-23 19:25:37 +0900

    :sparkles: add UserEntity

--- src/main/java/com/sparta/outsourcing/domain/user/entity/UserEntity.java
+++ src/main/java/com/sparta/outsourcing/domain/user/entity/UserEntity.java
@@ -1,5 +1,39 @@
 package com.sparta.outsourcing.domain.user.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "USER")
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String photo;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
 
 }
