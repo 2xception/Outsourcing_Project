7246e42fe566c51972efdfeccf857cfb73a217b5
TTPDE <bumjin0314@gmail.com>
2024-02-24 18:31:50 +0900

    PostEntity 추가

--- src/main/java/com/sparta/outsourcing/domain/post/entity/PostEntity.java
+++ src/main/java/com/sparta/outsourcing/domain/post/entity/PostEntity.java
@@ -1,9 +1,14 @@
 package com.sparta.outsourcing.domain.post.entity;
 
+import com.sparta.outsourcing.domain.user.entity.UserEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,5 +16,20 @@ import jakarta.persistence.Table;
 public class PostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
     private Long PostId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "views")
+    private Long views;
+
 }
