5ebfb0e8269ea951140a587519d3fa7146dd91ba
RebbitK <andxl9979@gmail.com>
2024-02-26 10:57:21 +0900

    :sparkles: Post Repository 추가

--- src/main/java/com/sparta/outsourcing/domain/post/repository/PostRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/post/repository/PostRepository.java
@@ -1,5 +1,17 @@
 package com.sparta.outsourcing.domain.post.repository;
 
+import com.sparta.outsourcing.domain.post.controller.model.Post;
+import com.sparta.outsourcing.domain.post.entity.PostEntity;
+
 public interface PostRepository {
+
+    Post findByPostId(Long id);
+
+    void save(PostEntity todoEntity);
+
+    void delete(Post post);
+
+    void update(Post post);
+
 
 }
--- src/main/java/com/sparta/outsourcing/domain/post/repository/PostRepositoryImpl.java
+++ src/main/java/com/sparta/outsourcing/domain/post/repository/PostRepositoryImpl.java
@@ -1,10 +1,34 @@
 package com.sparta.outsourcing.domain.post.repository;
 
+import com.sparta.outsourcing.domain.post.controller.model.Post;
+import com.sparta.outsourcing.domain.post.entity.PostEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepository{
+    private final PostJpaRepository postJpaRepository;
+
+    @Override
+    public Post findByPostId(Long id){
+        return Post.from(postJpaRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("선택하신 게시물은 존재하지 않습니다.")
+                ));
+    }
+    @Override
+    public void save(PostEntity postEntity){
+        postJpaRepository.save(postEntity);
+    }
+
+    @Override
+    public void delete(Post post){
+        postJpaRepository.delete(post.toEntity());
+    }
+
+    @Override
+    public void update(Post post){
+        postJpaRepository.saveAndFlush(post.toEntity());
+    }
 
 }
